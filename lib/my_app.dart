3e1003f6ce204fa799ba43a8cb84b12f8658ade6
Баттулга Бямбаа <68711572+Tulgaaaaaaaa@users.noreply.github.com>
2025-06-12 15:32:27 +0800

    title change

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         }
       },
       child: GetMaterialApp(
-        title: 'Movie app',
+        title: 'Unread Games',
         theme: ThemeData(
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
